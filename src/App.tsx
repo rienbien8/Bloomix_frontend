8ccef4a680d05d86a305a552500fe596fc617eda
kuwatsuru <bb.kosuke@gmail.com>
2025-08-16 04:38:49 +0900

    ホーム用の地図デモページ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SpotsList from "./pages/SpotsList";
+import HomeMap from "./pages/HomeMap";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/spots" element={<SpotsList />} />
+        <Route path="/map" element={<HomeMap />} />
       </Routes>
     </Router>
   );
